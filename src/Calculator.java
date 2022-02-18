b65be63a2c87bb27c6170476a4a4c7954953b687
Taylor Juve <taylor_juve@outlook.com>
2022-02-18 14:20:44 -0800

    Centers frame on screen. Class constants for frame dimensions.

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,11 +2,15 @@ import javax.swing.JFrame;
 
 public class Calculator {
 
+	public static final int FRAME_WIDTH = 300;
+	public static final int FRAME_HEIGHT = 500;
+	
 	public static void main(String[] args) {
-		JFrame calcWindow = new JFrame("Calculator");
-		calcWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		calcWindow.setSize(325, 500);
-		calcWindow.setVisible(true);
+		JFrame frame = new JFrame("Calculator");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		frame.setLocationRelativeTo(null); // centers frame on screen
+		frame.setVisible(true);
 	}
 
 }
