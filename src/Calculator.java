0808cd5e339208348418f3c2e9175f345aae44f9
Taylor Juve <taylor_juve@outlook.com>
2022-02-19 10:48:12 -0800

    Basic layout

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,15 +1,40 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class Calculator {
 
-	public static final int FRAME_WIDTH = 300;
-	public static final int FRAME_HEIGHT = 500;
+	public static final int FRAME_SIZE = 500;
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Calculator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-		frame.setLocationRelativeTo(null); // centers frame on screen
+		frame.setSize(FRAME_SIZE, FRAME_SIZE);
+		frame.setLocationRelativeTo(null); // centers frame in screen
+		
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		
+		JLabel result =
+				new JLabel("1,000,000,000,000,000", SwingConstants.RIGHT);
+		result.setFont(new Font("Courier", Font.BOLD, 36));
+		result.setPreferredSize(new Dimension(450, 100));
+		mainPanel.add(result, BorderLayout.NORTH);
+		
+		JPanel buttons = new JPanel(new GridLayout(0, 4, 2, 2));
+		for (int i = 0; i < 24; i++) {
+			buttons.add(new JButton("" + i));
+		}
+		mainPanel.add(buttons, BorderLayout.CENTER);
+		
+		frame.add(mainPanel);
+		
 		frame.setVisible(true);
 	}
 
